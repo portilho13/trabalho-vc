e650f243084ddb0978c8a5711c5878ad132d1dd6
Mario Portilho <junior.portilho2005@gmail.com>
2025-03-28 16:35:01 +0000

    basic content

---
+++ include/main.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+
+
+
+
+extern "C" {
+#include "vc.h"
+}
